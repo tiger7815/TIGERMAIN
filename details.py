1a1131c6b3ce67e6b73fd098ac24d222a9ce3295
darindaop <125014338+darindaop@users.noreply.github.com>
2023-04-26 21:28:43 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = 18516364
-api_hash = "2e90970ea9e3ad55a3d7566754274fae"
-bot_token = "2057624136:AAGqDUgyUmtNZjeaO-ySc-YYJhTomhS1hmA"
-
+api_id = "8130624"
+api_hash = "67a71560b00a31ffa692c67428f06d38"
+bot_token = "6000902354:AAGPPOzYoor5EVvDJzvvV2Qs52dfhvDEvy0"
+sudo_groups = -1001984099259
